eda4c9e8901efb2424704907f0d181fca66866d7
mapmapbear <m.n.jhop@163.com>
2024-09-03 00:02:08 +0800

    full bin dir and update xmake build script after build event

--- xmake.lua
+++ xmake.lua
@@ -12,4 +12,18 @@ target("sandbox")
     add_includedirs("3rd/Microsoft.Direct3D.D3D12.1.715.0-preview/build/native/include")
     if is_plat("windows") then
         add_syslinks("user32", "kernel32", "gdi32", "shell32", "dxgi", "d3d12", "d3dcompiler")
-    end    
+    end
+    add_rules("copyD3D12")
+
+rule("copyD3D12")
+    after_build(function (target)
+        -- if not os.exists("$(buildir)/windows/x64/debug") then 
+        --     os.mkdir("$(buildir)/windows/x64/debug")
+        --     print("LOG>>>>>>Create Output Folder Succseeful<<<<<<")
+        -- end
+        os.cp("$(scriptdir)/bin/*", "$(buildir)/windows/x64/debug")
+    end)
+    after_clean(function (target) 
+        os.rmdir("$(buildir)/windows/x64/debug/D3D12")
+        os.rm("$(buildir)/windows/x64/debug/shaders.hlsl")
+    end)
